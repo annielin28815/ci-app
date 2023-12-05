1a86f10ac9cb79cea3ff8d54bdebf2a2b998a2a2
Annie <annielin28815@gmail.com>
2023-12-05 16:52:06 +0800

    change the home and articles view by extend the default layout

--- app/Views/Articles/index.php
+++ app/Views/Articles/index.php
@@ -1,4 +1,8 @@
-<?= $this->include("header") ?>
+<?= $this->extend("layouts/default") ?>
+
+<?= $this->section("title") ?>Articles<?= $this->endSection() ?>
+
+<?= $this->section("content") ?>
   <h1>Article List</h1>
-</body>
-</html>
+<?= $this->endSection() ?>
+  
