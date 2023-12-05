89b851b45515c0430337b9910c575205bddf1a9c
Annie <annie_lin@books.com.tw>
2023-12-05 16:52:06 +0800

    change the home and articles view by extend the default layout

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,7 +6,6 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        return view("header", ["title" => "Home"])
-            .  view("Home/index.php");
+        return view("Home/index.php");
     }
 }
